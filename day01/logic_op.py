fe98370a00d0353563325c02d9a94861bc74cf64
jslee787 <wltntodrkr@naver.com>
2021-11-18 12:48:45 +0900

    day01

---
+++ day01/logic_op.py
@@ -0,0 +1,17 @@
+#논리 연산자
+
+x = 10
+y = -10
+
+print(x > 0)    #true
+print(y > 0)    #false
+print()
+
+print(x == 10)  #true
+print(x == y)   #false
+print(x != y)   #true
+print()
+
+print(x > 0 and y > 0)  #false
+print(x > 0 or y > 0)   #true
+print(not x > 0)        #false
